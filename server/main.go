7e80d57226ce197fc367c63b41a4a6bac971f77b
lu-sd <luhou.me@gmail.com>
2025-01-29 16:33:27 -0800

    go fiber

---
+++ server/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+type Todo struct {
+	ID   string `json:"id"`
+	Done bool   `json:"done"`
+	Body string `json:"body"`
+}
+
+func main() {
+	app := fiber.New()
+	todos := []Todo{}
+	// global State (todos list might not persist across multiple instances)
+	// If todos is a global slice, it will reset when the app restarts.
+	// Fix: Store the todos in a database (like SQLite, PostgreSQL, or Redis)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		// Why Use & (Pointer)?
+		// Efficient Memory Usage
+		// Instead of copying the entire struct, using a pointer means you’re passing around a reference to the data, which is more efficient.
+		//
+		//Modifying the Original Struct
+		// If BodyParser(todo) modifies todo, it directly modifies the struct in memory rather than a copy.
+		if err := c.BodyParser(todo); err != nil {
+			// The todo argument must be a pointer (&Todo{}) so that BodyParser can modify the struct directly.
+			return err
+			// parses the request body into a Go struct, return error if the request body can't be parsed
+		}
+		todo.ID = uuid.New().String()
+		todos = append(todos, *todo)
+		return c.JSON(todos)
+	})
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+		return c.JSON(todos)
+	})
+	app.Listen(":3000")
+}
